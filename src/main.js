e56f99cc95910ae5b4a0c45ec86076908a2d4809
GustavoGogola91 <90578528+GustavoGogola91@users.noreply.github.com>
2022-10-20 19:24:27 -0300

    ymask for year added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import "./css/index.css";
-import IMask from "imask";
+import IMask, { Masked } from "imask";
 
 const ccBgColor01 = document.querySelector(
   ".cc-bg svg > g g:nth-child(1) path"
@@ -26,3 +26,21 @@ const securityCodePattern = {
   mask: "0000",
 };
 const securityCodeMasked = IMask(securityCode, securityCodePattern);
+
+const expirationDate = document.querySelector("#expiration-date");
+const expirationDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2),
+    },
+  },
+};
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern);
